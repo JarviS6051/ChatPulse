2e4f7226f77f023c22d0a82a0423d367602b0d36
JarviS6051 <yashb150603@gmail.com>
2025-01-29 11:07:11 +0530

    Done

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
 import { Users } from 'lucide-react';
@@ -20,7 +20,7 @@ const Sidebar = () => {
 
   return (
     <aside className='h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200'>
-        <div className='border-b border-base-30 w-full p-5'> 
+        <div className='border-b border-base-300 w-full p-5'> 
             <div className='flex items-center gap-2'>
                 <Users className='size-6'/>
                 <span className='font-medium hidden lg:block'>Contacts</span>
